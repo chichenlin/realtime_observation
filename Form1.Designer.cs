0a6ba343f0cbe367910c46f7ccdd175c2d206634
chichenlin <CC.Lin@irti.org.tw>
2019-11-06 11:37:39 +0800

    test

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace test
+{
+    partial class plotfigure
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.time_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.stopbutton = new System.Windows.Forms.Button();
+            this.FFT_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.time_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FFT_chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // time_chart
+            // 
+            chartArea1.AxisX.Title = "Time";
+            chartArea1.Name = "ChartArea1";
+            this.time_chart.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.time_chart.Legends.Add(legend1);
+            this.time_chart.Location = new System.Drawing.Point(12, 12);
+            this.time_chart.Name = "time_chart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.time_chart.Series.Add(series1);
+            this.time_chart.Size = new System.Drawing.Size(618, 327);
+            this.time_chart.TabIndex = 0;
+            this.time_chart.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "time";
+            this.time_chart.Titles.Add(title1);
+            this.time_chart.Click += new System.EventHandler(this.time_chart_Click);
+            // 
+            // startbutton
+            // 
+            this.startbutton.Location = new System.Drawing.Point(658, 12);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(75, 23);
+            this.startbutton.TabIndex = 1;
+            this.startbutton.Text = "start";
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // stopbutton
+            // 
+            this.stopbutton.Location = new System.Drawing.Point(658, 12);
+            this.stopbutton.Name = "stopbutton";
+            this.stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.stopbutton.TabIndex = 2;
+            this.stopbutton.Text = "stop";
+            this.stopbutton.UseVisualStyleBackColor = true;
+            this.stopbutton.Click += new System.EventHandler(this.stopbutton_Click);
+            // 
+            // FFT_chart
+            // 
+            chartArea2.AxisX.Title = "Frequency";
+            chartArea2.Name = "ChartArea1";
+            this.FFT_chart.ChartAreas.Add(chartArea2);
+            legend2.Enabled = false;
+            legend2.Name = "Legend1";
+            this.FFT_chart.Legends.Add(legend2);
+            this.FFT_chart.Location = new System.Drawing.Point(12, 345);
+            this.FFT_chart.Name = "FFT_chart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.FFT_chart.Series.Add(series2);
+            this.FFT_chart.Size = new System.Drawing.Size(618, 327);
+            this.FFT_chart.TabIndex = 3;
+            this.FFT_chart.Text = "chart2";
+            title2.Name = "Title1";
+            title2.Text = "FFT";
+            this.FFT_chart.Titles.Add(title2);
+            // 
+            // plotfigure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 669);
+            this.Controls.Add(this.FFT_chart);
+            this.Controls.Add(this.stopbutton);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.time_chart);
+            this.Name = "plotfigure";
+            this.Text = "figure";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.time_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FFT_chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart time_chart;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Button stopbutton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart FFT_chart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NationalInstruments.DAQmx;
+using NationalInstruments;
+using MathNet.Numerics;
+using System.IO;
+using System.Diagnostics;
+
+namespace test
+{
+    public partial class plotfigure : Form
+    {
+        private AnalogMultiChannelReader analogInReader;
+        private AIExcitationSource excitationSource;
+        private AIAccelerometerSensitivityUnits sensitivityUnits;
+        private AITerminalConfiguration terminalConfiguration;
+        private AnalogEdgeStartTriggerSlope triggerSlope;
+        private AICoupling inputCoupling;
+
+        private NationalInstruments.DAQmx.Task myTask;
+        private NationalInstruments.DAQmx.Task runningTask;
+        private AsyncCallback analogCallback;
+        private AnalogWaveform<double>[] data;
+
+        int indexP;
+        double SPmax;
+        int iii;
+        public StreamWriter SW_RMSData;
+        public StreamWriter SW_State;
+        public StreamWriter SW_RawDataX;
+        public StreamWriter SW_FFTDataX;
+
+        public plotfigure()
+        {
+            InitializeComponent();
+            startbutton.Visible = true;
+            stopbutton.Visible = false;
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            
+
+            
+            Refresh();
+            startbutton.Visible = false;
+            stopbutton.Visible = true;
+            time_chart.Series[0].Points.Clear();
+            StartDAQ(8000);
+            
+
+
+
+
+
+        }
+
+        private void stopbutton_Click(object sender, EventArgs e)
+        {
+            StopDAQ();
+            startbutton.Visible = true;
+            stopbutton.Visible = false;
+            Refresh();
+        }
+
+        public void StartDAQ(double a)
+        {
+
+            triggerSlope = AnalogEdgeStartTriggerSlope.Rising;
+            sensitivityUnits = AIAccelerometerSensitivityUnits.MillivoltsPerG;
+            terminalConfiguration = (AITerminalConfiguration)(-1);
+            excitationSource = AIExcitationSource.Internal;
+            inputCoupling = AICoupling.AC;
+
+            myTask = new NationalInstruments.DAQmx.Task();
+            AIChannel aiChannel;
+
+            SPmax = a;
+            double Vmin = -5;
+            double Vmax = 5;
+            double sen = 100;
+            double EVN = 0.004;
+            double[] chan = new double[4] { 1, 1, 0, 0 };
+
+            SW_RMSData = new StreamWriter(System.Environment.CurrentDirectory + "\\logData\\RMSData.txt");
+            SW_State = new StreamWriter(System.Environment.CurrentDirectory + "\\logData\\State.txt");
+            SW_RawDataX = new StreamWriter(System.Environment.CurrentDirectory + "\\logData\\RawDataX.txt");
+            SW_FFTDataX = new StreamWriter(System.Environment.CurrentDirectory + "\\logData\\FFTDataX.txt");
+
+            for (int i = 0; i < chan.Length; i++)
+            {
+                if (chan[i] == 1)
+                {
+                    //擷取卡的名稱要改
+                    aiChannel = myTask.AIChannels.CreateAccelerometerChannel("cDAQ1Mod1/ai" + Convert.ToString(i), "",
+                        terminalConfiguration, Vmin, Vmax, sen, sensitivityUnits, excitationSource,
+                        EVN, AIAccelerationUnits.G);
+                    aiChannel.Coupling = inputCoupling;
+                }
+
+            }
+
+            myTask.Timing.ConfigureSampleClock("", Convert.ToDouble(12800),
+                SampleClockActiveEdge.Rising, SampleQuantityMode.ContinuousSamples, Convert.ToInt32(1280));
+
+            myTask.Control(TaskAction.Verify);
+
+            runningTask = myTask;
+            analogInReader = new AnalogMultiChannelReader(myTask.Stream);
+            analogCallback = new AsyncCallback(AnalogInCallback);
+
+
+
+            analogInReader.SynchronizeCallbacks = true;
+            analogInReader.BeginReadWaveform(Convert.ToInt32(1280), analogCallback, myTask);
+
+        }
+
+        public void StopDAQ()
+        {
+            // Dispose of the task
+            SW_RMSData.Dispose();
+            SW_State.Dispose();
+            SW_RawDataX.Dispose();
+            SW_FFTDataX.Dispose();
+
+            runningTask = null;
+            myTask.Dispose();
+
+            indexP = 0;
+            iii = 0;
+        }
+        private void AnalogInCallback(IAsyncResult ar)
+        {
+            if (runningTask != null && runningTask == ar.AsyncState)
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                // Read the available data from the channels
+                data = analogInReader.EndReadWaveform(ar);
+
+                double[] vecTime = new double[data[0].SampleCount];
+
+                double[] d00 = data[0].GetRawData();
+                double[] d10 = data[1].GetRawData();
+                //
+                time_chart.Series[0].Points.Clear();
+                for (int i = 0; i < d00.Length; i++)
+                {
+                    time_chart.Series[0].Points.AddXY(vecTime[i], d00[i]);
+                }
+                //save rawdata
+                for (int i = 0; i < d00.Length; i++)
+                {
+                    SW_RawDataX.Write(i);
+                    SW_RawDataX.Write(",");
+                    SW_RawDataX.Write(vecTime[i]);
+                    SW_RawDataX.Write(",");
+                    SW_RawDataX.WriteLine(d00[i]);
+                }
+                //
+
+                double rms = rootMeanSquare(d00);
+                Console.WriteLine(rms) ;
+
+                //
+                PrecisionDateTime[] T = data[0].GetPrecisionTimeStamps();
+
+                double[] d0 = new double[d00.Length];
+                double[] d1 = new double[d10.Length];
+
+                for (int i = 0; i < d0.Length; i++)
+                {
+                    d0[i] = (d00[i] - d00.Average()) * 10;
+                    d1[i] = (d10[i] - d10.Average()) * 10;
+                }
+
+                for (int i = 0; i < data[0].SampleCount; i++)
+                {
+                    vecTime[i] = T[i].TimeOfDay.TotalSeconds;
+                }
+                double varFs = 1 / ((vecTime[data[0].SampleCount - 1] - vecTime[0]) / data[0].SampleCount);
+
+                //int indexMaterial = CUTeffiForm.indexMaterial;
+
+                double rms_xdata = rootMeanSquare(d0) * 10;
+                double rms_ydata = rootMeanSquare(d1) * 10;
+                double rms_vibration = Math.Sqrt(Math.Pow(rms_xdata, 2) + Math.Pow(rms_ydata, 2));
+
+
+                ////////////////////////////FFT////////////////////////////////////////
+                double[] d0Copy = new double[d0.Length * 10];
+
+                for (int i = 0; i < d0.Length; i++)
+                {
+                    d0Copy[i] = d0[i];
+                }
+                double[] realdata = d0Copy;
+                double[] imagdata = new double[d0Copy.Length];
+
+                int numFFT = Convert.ToInt16(Math.Floor(Convert.ToDecimal(d0Copy.Length / 2)));
+                double[] vecFFT = new double[numFFT];
+                double[] vecFqtmp = new double[numFFT];
+
+                for (int i = 1; i < numFFT; i++)
+                {
+                    vecFqtmp[i] = vecFqtmp[i - 1] + (varFs / d0Copy.Length);
+                }
+
+                MathNet.Numerics.IntegralTransforms.Fourier.Forward(realdata, imagdata, MathNet.Numerics.IntegralTransforms.FourierOptions.Matlab);
+
+                for (int i = 0; i < numFFT; i++)
+                {
+                    vecFFT[i] = Math.Sqrt(Math.Pow(realdata[i], 2) + Math.Pow(imagdata[i], 2));
+                }
+
+                FFT_chart.Series[0].Points.Clear();
+
+                for (int j = 0; j < 600; j++)
+                {
+                    FFT_chart.Series[0].Points.AddXY(j, vecFFT[j]);
+                }
+                //save FFT data
+                for (int j = 0; j < 600; j++)
+                {
+                    SW_FFTDataX.Write(j);
+                    SW_FFTDataX.Write(",");
+                    SW_FFTDataX.WriteLine(vecFFT[j]);
+                }
+                //
+                analogInReader.BeginMemoryOptimizedReadWaveform(Convert.ToInt32(1280),
+                    analogCallback, myTask, data);
+
+            }
+        }
+
+        private static double rootMeanSquare(double[] x)
+        {
+            double sum = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum += (x[i] * x[i]);
+            }
+            return Math.Sqrt(sum / x.Length);
+        }
+
+        private void time_chart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
